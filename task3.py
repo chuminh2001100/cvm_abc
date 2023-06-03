9ee00ac7d7d75dd1486cb43db8d763ef39d00d9d
chuminh2001100 <129584626+chuminh2001100@users.noreply.github.com>
2023-06-03 10:37:13 +0700

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,7 @@
+class Task3:                                                                                                                                           
+    def __init__(self):                                                                                                                                
+        print("Adu vjp")                                                                                                                           
+        return                                                                                                                                         
+                                                                                                                                                       
+    def Task3_Run(self):                                                                                                                               
+        print("Task 3 is activated!!!!")
